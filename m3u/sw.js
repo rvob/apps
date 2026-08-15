2d77fab8eea4b280c71ac46fff89cb857c37cd3b
rvob <115838880+rvob@users.noreply.github.com>
2026-08-15 20:18:52 -0300

    Add files via upload

---
+++ m3u/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'links-m3u-v1';
+const ASSETS_TO_CACHE = [
+  './m3u.html',
+  './manifest.json',
+  './favicon.svg',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((response) => {
+            // guarda uma cópia no cache para uso offline
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+            return response;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
